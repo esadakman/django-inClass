5258e65a902e1e69c0d7d95787cb01ccf2e98622
esadakman <esadd26@gmail.com>
2022-08-24 20:29:33 +0300

    typo

--- session_007_Functional_Views_Templates/prj/settings.py
+++ session_007_Functional_Views_Templates/prj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-jwlkguc+%vy67$4w=62h(jfj)xt&wo9s+*%xr8-ssc%c3msa0l"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
