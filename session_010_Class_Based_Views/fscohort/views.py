cb2728c838b76ccbafe2c691d6072f5b59f5042e
esadakman <esadd26@gmail.com>
2022-08-27 21:52:35 +0300

    session 10 uploaded

---
+++ session_010_Class_Based_Views/fscohort/views.py
@@ -0,0 +1,139 @@
+from django.views.generic import DeleteView
+from django.views.generic import UpdateView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.views.generic import DetailView
+from django.views.generic import ListView
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from .forms import StudentForm
+from .models import Student
+from django.views.generic import TemplateView
+# Create your views here.
+
+
+def home(request):
+    return render(request, "fscohort/home.html")
+
+
+class HomeView(TemplateView):
+    template_name = "fscohort/home.html"
+
+
+def student_list(request):
+
+    students = Student.objects.all()
+
+    context = {
+        "students": students
+    }
+
+    return render(request, "fscohort/student_list.html", context)
+
+
+class StudentListView(ListView):
+    model = Student
+    # default template name : # app/modelname_list.html
+    # this fits our template name no need to use this time
+    # template_name = "fscohort/student_list.html"
+    context_object_name = 'students'  # default context name : object_list
+    paginate_by = 10
+    # queryset = Student.objects.all() or filter
+    # get_queryset method for more owerfull filtering ( we can put data into get_context_data method and send template )
+
+
+def student_add(request):
+    form = StudentForm()
+
+    if request.method == "POST":
+        form = StudentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("list")
+
+    context = {
+
+        "form": form
+    }
+
+    return render(request, "fscohort/student_add.html", context)
+
+
+class StudentCreateView(CreateView):
+    model = Student
+    form_class = StudentForm
+    # default name app/modelname_form.html
+    template_name = "fscohort/student_add.html"
+    success_url = reverse_lazy("list")
+
+    def form_valid(self, form):
+        self.object = form.save()
+        if not self.object.number:
+            self.object.number = 999
+        self.object.save()
+        return super().form_valid(form)
+
+
+def student_detail(request, id):
+    student = Student.objects.get(id=id)
+    context = {
+        "student": student
+    }
+
+    return render(request, "fscohort/student_detail.html", context)
+
+
+class StudentDetailView(DetailView):
+    model = Student
+    pk_url_kwarg = 'id'
+
+
+def student_update(request, id):
+
+    student = Student.objects.get(id=id)
+
+    form = StudentForm(instance=student)
+
+    if request.method == "POST":
+        form = StudentForm(request.POST, request.FILES, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect("list")
+
+    context = {
+
+        "student": student,
+        "form": form
+    }
+
+    return render(request, "fscohort/student_update.html", context)
+
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    form_class = StudentForm
+    template_name = "fscohort/student_update.html"  # default app/modelname_form.html
+    success_url = '/student_list/'  # 'reverse_lazy("list")
+    # pk_url_kwarg = 'id'
+
+
+def student_delete(request, id):
+
+    student = Student.objects.get(id=id)
+
+    if request.method == "POST":
+
+        student.delete()
+        return redirect("list")
+
+    context = {
+        "student": student
+    }
+    return render(request, "fscohort/student_delete.html", context)
+
+
+class StudentDeleteView(DeleteView):
+    model = Student
+    # default app/modelname_confirm_delete.html
+    template_name = 'fscohort/student_delete.html'
+    success_url = reverse_lazy("list")
